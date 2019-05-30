1361a24558ea25f0cd808e59448cdd59e554c8f4
nuriatabanera <nurss.ts@gmail.com>
2019-05-31 00:45:12 +0200

    appnuriasunny

--- geolocation.js
+++ geolocation.js
@@ -3,7 +3,7 @@ $(document).on("mobileinit", function(){
 
 	$(function(){
 
-		 $("#main").on("pageinit", function(){
+		$("#main").on("pageinit", function(){
 
 		 	navigator.geolocation.getCurrentPosition(success);
 
@@ -12,27 +12,50 @@ $(document).on("mobileinit", function(){
 				let long= position.coords.longitude;
 				//alert('Your latitude is: '+lat+'and longitude is '+long); 
 
+				let url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=metric&APPID=055cbdecc7321d265bcdddf979cb43af";
+				
+				$.getJSON(url).done(function(response){     
+			
+					let html = "<a href='#detalle' class='ui-btn ciudad'><div id='nombreciudad'>"+response.name+"</div><div id='temperaturaciudad'>"+response.main.temp+"º</div>";
+
+					$("#menu").append(html);
+				});
 			}
 
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=055cbdecc7321d265bcdddf979cb43af", function(resp){
 
-				let lat= resp;
-				let long= resp;
 
-				$.each( function(index, item) {
-					$("#ciudad1").append("city.name");
 
-				});
+				
 
-	
+
+				 let ciudades = JSON.parse(localStorage.getItem('elementos'));
+				 let temps = JSON.parse(localStorage.getItem('temps'));
+				 if(ciudades != null){
+					// si hay - recorrer el array y pintar datos en la lista de ciudades - 
+					$.each(ciudades, function(ind, val){ 
+						//en #menu hasr un append (igual que en el clic) 
+
+						let ciudad = "<a href='#detalle' class='ui-btn ciudad'><div id='nombreciudad'>"+ciudades[ind]+"</div><div id='temperaturaciudad'>"+temps[ind]+"º</div>";
+						//let ciudad = "<a href='#detalle' class='ui-btn ciudad'><div id='nombreciudad'>"+ciudades[ind]+"</div>";
+				 		$("#menu").append(ciudad);
+
+					});
+					
+
+				 }
+				
+
+			
+				
+	///////////////
+			
+			
+
 		});
 
-		
 
+///////////// BUSCADOR
 
-		});
-
-///////////
 
 
 		 $("#buscador").on("pageinit", function(){
@@ -55,7 +78,7 @@ $(document).on("mobileinit", function(){
 		            })
 		            .then( function ( response ) {
 		                $.each( response, function ( i, val ) {
-		                    html += "<li>" + val + "</li>";
+		                    html += "<li class='elementos'><a href='#main'>" + val + "</a></li>";
 		                });
 		                $ul.html( html );
 		                $ul.listview( "refresh" );
@@ -67,21 +90,88 @@ $(document).on("mobileinit", function(){
 		 });
 
 		
-			/////////	
+/////////	
+
+// click en los resultados del buscador
+		 $("#autocomplete").on("click", ".elementos",function(){
+			//let tmperatura;
+			//console.log($(this).text());
+			let nomciudad = $(this).text();
+				//let ciudad= "<a href='#detalle' class='ui-btn ciudad'><div id='nombreciudad'>"+response.name+"</div><div id='temperaturaciudad'>"+response.main.temp+"º</div>";
+			
+				//$("#menu").append(ciudad);
+
+				let url = "http://api.openweathermap.org/data/2.5/weather?q="+nomciudad+"&units=metric&APPID=055cbdecc7321d265bcdddf979cb43af";
+	
+				$.getJSON(url).done(function(response){  
+
+					let ciudad= "<a href='#detalle' class='ui-btn ciudad'><div id='nombreciudad'>"+response.name+"</div><div id='temperaturaciudad'>"+response.main.temp+"º</div>";
+							 		
+					let tmperatura = response.main.temp;
+					 $("#menu").append(ciudad);
+					 
+					 let ciudades = JSON.parse(localStorage.getItem('elementos'));
+					 let temps = JSON.parse(localStorage.getItem('temps'));
+					 if(ciudades == null){
+						ciudades = [];
+						temps = [];
+					 }
+					
+				 //  añadir ciudad al array(ciudades)
+					 ciudades.push(nomciudad);
+					 temps.push(tmperatura);
+				  //guardar la ciudad en localStorage	
+					localStorage.setItem('elementos', JSON.stringify(ciudades));
+					localStorage.setItem('temps', JSON.stringify(temps));
+
+		 		});
+				 
+			
+		
 
 
 
+
+
+		 });
 
 	});
 
 });
 
-//pedir localizacion a la api de html 5 
-//hacer peticion json a web del tiempo
-//con el response, coger ciudad y grados
-//pintar ciudad y grados en el
 
 
 
 
-//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=055cbdecc7321d265bcdddf979cb43af
+
+
+
+
+
+			
+$(document).on("click", ".ciudad",function(){
+
+	console.log($(this).find( "#nombreciudad" ).text());
+	let nomciudad=$(this).find( "#nombreciudad" ).text();
+
+
+	//navigator.geolocation.getCurrentPosition(success);
+
+		
+		
+		let url = "http://api.openweathermap.org/data/2.5/weather?q="+nomciudad+"&units=metric&APPID=055cbdecc7321d265bcdddf979cb43af";
+
+		$.getJSON(url).done(function(response){  
+			console.log("llhkjh");
+
+		let html = "<div id='box1'><div id='texto'><p>¿Qué tiempo hace?</p><h1>"+response.name+"</h1><h2>"+response.main.temp+"º</h2><p>Temp. Mínima  "+response.main.temp_min+"º</p><p>Temp. Máxima  "+response.main.temp_max+"º</p><h4>¿Hay sol?</h4></div></div>";
+							
+		$("#detalleciudad").append(html);
+
+
+		});
+
+
+
+});
+
